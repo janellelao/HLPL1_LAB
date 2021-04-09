20a065d344efba570301bddead99fad79209ae27
Janelle Lao <78431060+janellelao@users.noreply.github.com>
2021-04-09 17:27:05 +0200

    Update 10drill.cpp

--- Drill10/10drill.cpp
+++ Drill10/10drill.cpp
@@ -42,8 +42,8 @@ ostream& operator << (ostream& os, Point& p)
 int main()
 try { 
      // 10.2 prompt user and store points in vector
-     cout << "Enter seven pairs of x,y points please!\n" <<
-         "Open and close each point with () and seprate them with a ','";
+     cout << "Enter seven pairs of (x,y) points please!\n" ;
+         
      
      vector<Point> original_points;
      
@@ -59,24 +59,27 @@ try {
          
          
       // 10.4 open ofstream to output points onto a file
-      string name; "mydata.txt";
-      
-      ofstream ost{name};
-      for (Point p: original_points)
-        ost << p; 
-        ost.close();
+      string ofname= "mydata.txt";
+      ofstream ofs{ofname}
         
-      if (!ost) error("cannot open output file", name);
-      
-      // code compiles but it keeps getting an error message for the output bit, no clue why
+      if (!ofs) error("cannot open output file", ofname);
+   
+      for (Point p: original_points)
+        ofs << p << '\n'; 
+        ofs.close()
          
       // 10.5 close ofstream open ifstream, store points in vector called processed points
       vector<Point> processed_points;
-      ifstream ist{name};
-      
+   
+      string ifname= "mydata.txt";
+      ifstream ifs{ifname};
+     
+      if (!ifs) error("cannot open input file", ifname);
+   
       for (Point p : processed_points)
         cout << p << '\n';
-      if (!ist) error("cannot open input file", name);
+   
+   
       
       // 10.7 compare the two vectors?
     if (original_points.size() != processed_points.size())
@@ -87,9 +90,4 @@ catch(exception& e)
 {
     cerr << e.what() << '\n';
     return 1;
-}
-catch(...)
-{
-    cerr << "Unknown error!\n";
-    return 2;
 }
